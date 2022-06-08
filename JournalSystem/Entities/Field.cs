fc78eca11218f8677721bb19be5448d328b6deb5
Eyob Bezu <jobs.bezu@gmail.com>
2022-06-08 19:51:00 +0300

    apply migration

--- JournalSystem/Entities/Field.cs
+++ JournalSystem/Entities/Field.cs
@@ -6,8 +6,8 @@ namespace JournalSystem.Entities
     public class Field
     {
         public Guid Id { get; set; }
-        public string FieldName { get; set; }
-        public string Specialization { get; set; }
+        //public string FieldName { get; set; }
+        //public string Specialization { get; set; }
         public IEnumerable<User> Users { get; set; }
     }
 }
--- JournalSystem/Entities/Institution.cs
+++ JournalSystem/Entities/Institution.cs
@@ -6,11 +6,11 @@ namespace JournalSystem.Entities
     public class Institution
     {
         public Guid InstitutionId { get; set; }
-        public string Institutiion_Name { set; get; }
-        public string Location { get; set; }
-        public string Institution_Email { get; set; }
-        public string Institution_Phone { get; set; }
-        public string Institution_website { get; set; }
+        //public string Institutiion_Name { set; get; }
+        //public string Location { get; set; }
+        //public string Institution_Email { get; set; }
+        //public string Institution_Phone { get; set; }
+        //public string Institution_website { get; set; }
         public IEnumerable<User> Users { get; set; }
     }
 }

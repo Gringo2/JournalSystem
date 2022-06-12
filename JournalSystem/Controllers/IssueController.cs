625f5bceacd0378fbfc3982c44001b6a20aa1c17
ElihuG <getachewelihu@gmail.com>
2022-06-13 02:22:42 +0300

    Built Issue entity upto controller

---
+++ JournalSystem/Controllers/IssueController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using JournalSystem.Entities;
+using JournalSystem.Models;
+using JournalSystem.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JournalSystem.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class IssueController : ControllerBase
+        {
+            private readonly IRepository<Issue> _issueRepo;
+            private readonly IMapper _mapper;
+
+            public IssueController(IRepository<Issue> issueRepo, IMapper mapper)
+            {
+                _issueRepo = issueRepo;
+                _mapper = mapper;
+            }
+
+            [HttpGet]
+            public async Task<ActionResult<IEnumerable<IssueDto>>> GetAll()
+            {
+                IEnumerable<Issue> issues = await _issueRepo.GetAll();
+                var map = _mapper.Map<IEnumerable<IssueDto>>(issues);
+                return Ok(map);
+            }
+
+            [HttpGet("GetIssueByID/{Id}")]
+            public async Task<ActionResult<IssueDto>> GetIssueByID(Guid Id)
+            {
+
+                var response = await _issueRepo.GetById(Id);
+                return Ok(_mapper.Map<IssueDto>(response));
+            }
+
+            [HttpPost("SubmitIssue")]
+            public async Task<ActionResult<IssueDto>> SubmitIssue(IssueDto issue)
+            {
+                var map = _mapper.Map<Issue>(issue);
+                await _issueRepo.Insert(map);
+                return CreatedAtAction(nameof(GetIssueByID), new { IssueId = issue.Id }, issue);
+            }
+
+            [HttpPut("UpdateIssue/{Id}")]
+            public async Task<ActionResult<IssueDto>> UpdateIssue(IssueDto issue)
+            {
+                var map = _mapper.Map<Issue>(issue);
+                await _issueRepo.Update(map);
+                return Ok(_mapper.Map<IssueDto>(await _issueRepo.GetById(issue.Id)));
+            }
+
+            [HttpDelete("DeleteIssue/{cId}")]
+            public async Task<ActionResult<IssueDto>> DeleteIssue(Guid cId)
+            {
+                var deletable = await _issueRepo.Delete(cId);
+                var map = _mapper.Map<IssueDto>(deletable);
+
+                if (deletable == null)
+                {
+                    return NotFound();
+                }
+                return map;
+            }
+        }
+}
---
+++ JournalSystem/Models/IssueDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JournalSystem.Models
+{
+    public class IssueDto
+    {
+        public Guid Id { get; set; }
+        public string JournalName { get; set; }
+        public int Volume { get; set; }
+        public int Issue_No { get; set; }
+        public string Publisher { get; set; }
+        public string ImageUrl { get; set; }
+        public string FilePath { get; set; }
+        public DateTime YearPublished { get; set; }
+    }
+}

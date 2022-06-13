0df207430bc98802d221bfed19bba9eb868d1cf2
ElihuG <getachewelihu@gmail.com>
2022-06-13 08:28:02 +0300

    Changed seeded categories and topics, and fixed issues table migration

--- JournalSystem/Migrations/20220613052615_AddIssuesTable.Designer.cs
+++ JournalSystem/Migrations/20220613052615_AddIssuesTable.Designer.cs
@@ -4,14 +4,16 @@ using JournalSystem.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JournalSystem.Migrations
 {
     [DbContext(typeof(DataDbContext))]
-    partial class DataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220613052615_AddIssuesTable")]
+    partial class AddIssuesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- JournalSystem/Seeders/TopicSeeder.cs
+++ JournalSystem/Seeders/TopicSeeder.cs
@@ -17,10 +17,10 @@ namespace JournalSystem.Seeders
 
         public void SeedData()
         {
-            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"), TopicName = "Bio-Tech", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
-            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5817-4562-b3fc-2c963f66afa6"), TopicName = "Languages", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
-            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5717-4562-b3ec-2c963f66afa6"), TopicName = "Astronomy", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
-            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5717-4562-b3fc-2c023f66afa6"), TopicName = "Pharmacy", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
+            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"), TopicName = "Allergies", Description = "Allergy is a leading, open access, peer reviewed journal covering all areas of research from surgical techniques to diagnostic dilemmas, as well as treatment options and pathophysiologic investigations in sinonasal diseases, as well as asthma and other allergic disorders.", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
+            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5817-4562-b3fc-2c963f66afa6"), TopicName = "Hospitalizations", Description = "Journal of Hospital and Healthcare Administration is an online open access journal covers theories, methods and applications in hospital and healthcare administration. This journal includes the leadership, management and administration of public health systems, hospitals and hospital networks.", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
+            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5717-4562-b3ec-2c963f66afa6"), TopicName = "Injuries", Description = "Injury Epidemiology is a pioneering, open access journal publishing cutting-edge epidemiologic studies of both intentional and unintentional injuries.", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
+            AddNewType(new Topic { TopicId = new Guid("3fa85f64-5717-4562-b3fc-2c023f66afa6"), TopicName = "Surgeries", Description = "Surgery journal features the very best in clinical and laboratory-based research on all aspects of surgical procedures and interventions. Patient care around the peri-operative period and patient outcomes post surgery are key topics for the journal.", CategoryId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6") });
             _context.SaveChanges();
         }
 
